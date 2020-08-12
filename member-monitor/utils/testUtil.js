94cc1d61d79eb1725e164e6c6f4f04582893bf1f
นที สังข์ทองงาม <natee.sun@gmail.com>
2020-08-12 20:07:08 +0700

    add member-monitor in crm online web project

---
+++ member-monitor/utils/testUtil.js
@@ -0,0 +1,4 @@
+const TextUtil = require('./TextUtil')
+
+console.log('result:', TextUtil.convUnicode2Ascii('Nathee Sungthongngam'))
+// console.log('result:', TextUtil.convAscii2Unicode('¹·Õ ÊÑ§¢ì·Í§§ÒÁ'))
