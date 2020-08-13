7878d5ab8452b8055f098475c0e4e29ec9e687aa
นที สังข์ทองงาม <natee.sun@gmail.com>
2020-08-13 20:07:37 +0700

    update react-boilerplate to my project

--- webdialy-online/internals/webpack/webpack.prod.babel.js
+++ webdialy-online/internals/webpack/webpack.prod.babel.js
@@ -116,20 +116,20 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
+      name: 'Web Dialy Online',
       short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      description: 'My Web Dialy Online-based project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/icon-512x512.ico'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/icon-512x512.ico'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
