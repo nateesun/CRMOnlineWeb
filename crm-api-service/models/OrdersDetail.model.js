ccaaa466338a58c88b14cad6752ad39245bb41e7
นที สังข์ทองงาม <natee.live@gmail.com>
2020-10-11 15:18:54 +0700

    update col1 to real column

--- crm-api-service/models/OrdersDetail.model.js
+++ crm-api-service/models/OrdersDetail.model.js
@@ -66,11 +66,13 @@ module.exports = (db) => {
     console.log("update method start:")
     return new Promise(async (resolve, reject) => {
       try {
-        const query = `UPDATE ${table_name} SET col1=?, col2=?, col3=? WHERE uuid_index=? `
+        const query = `UPDATE ${table_name} 
+        SET product_code=?, qty=?, options=?, special_text=? WHERE uuid_index=? `
         const result = await pool.query(query, [
-          data.col1,
-          data.col2,
-          data.col3,
+          data.product_code,
+          data.qty,
+          data.options,
+          data.special_text,
           data.uuid_index,
         ])
         resolve({ status: "Success", data: JSON.stringify(result) })
