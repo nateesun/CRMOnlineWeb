82b2a1a3cc6bff4fa9095295b470ab6786f6ef35
นที สังข์ทองงาม <natee.live@gmail.com>
2020-10-28 17:38:22 +0700

    migrate script

--- crm-api-service/internals/generators/database/db.sql
+++ crm-api-service/internals/generators/database/db.sql
@@ -1,20 +1,20 @@
-CREATE TABLE `bank` (
+CREATE TABLE IF NOT EXISTS `bank` (
   `uuid_index` varchar(36) NOT NULL,
   `code` varchar(5) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `code_no` varchar(100) DEFAULT NULL,
   `postfix` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `branch` (
+CREATE TABLE IF NOT EXISTS `branch` (
   `uuid_index` varchar(36) NOT NULL,
-  `code` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
+  `code` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `map_latitude` varchar(100) DEFAULT NULL,
   `map_longitude` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `carts` (
+CREATE TABLE IF NOT EXISTS `carts` (
   `uuid_index` varchar(36) NOT NULL,
   `cart_no` varchar(100) DEFAULT NULL,
   `cart_create_date` datetime DEFAULT NULL,
@@ -35,9 +35,9 @@ CREATE TABLE `carts` (
   `transfer_amount` float(10,2) DEFAULT NULL,
   `slip_path` varchar(100) DEFAULT NULL,
   `emp_update_date` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `carts_detail` (
+CREATE TABLE IF NOT EXISTS `carts_detail` (
   `uuid_index` varchar(36) NOT NULL,
   `cart_no` varchar(100) DEFAULT NULL,
   `product_code` varchar(20) DEFAULT NULL,
@@ -49,9 +49,9 @@ CREATE TABLE `carts_detail` (
   `options` varchar(100) DEFAULT NULL,
   `special_text` varchar(100) DEFAULT NULL,
   `point` int(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `company` (
+CREATE TABLE IF NOT EXISTS `company` (
   `uuid_index` varchar(36) NOT NULL,
   `code` varchar(100) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
@@ -64,16 +64,23 @@ CREATE TABLE `company` (
   `order_size_running` int(10) DEFAULT NULL,
   `cart_running` int(5) DEFAULT NULL,
   `cart_prefix` varchar(3) DEFAULT NULL,
-  `cart_size_running` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `cart_size_running` int(10) DEFAULT NULL,
+  `member_register_point` int(3) DEFAULT NULL
+);
+INSERT INTO `company`
+(uuid_index, code, name, line_official_id, member_running, prefix_running, size_running, order_running, order_prefix, order_size_running, cart_running, cart_prefix, cart_size_running, member_register_point)
+VALUES('3eeec383-0034-4213-bc93-e5e2ce608055', 'C001', 'MyCompany Limited 2021', '@mycom2021', 1, 'MB', 5, 1, 'OD', 5, 1, 'SP', 5, 10);
 
-CREATE TABLE `login` (
+CREATE TABLE IF NOT EXISTS `login` (
   `member_active` char(1) NOT NULL DEFAULT 'Y',
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
+INSERT INTO `login`
+(member_active, username, password)
+VALUES('Y', 'softpos@gmail.com', 'MTExMTEx');
 
-CREATE TABLE `member` (
+CREATE TABLE IF NOT EXISTS `member` (
   `code` varchar(36) NOT NULL DEFAULT '',
   `company_code` varchar(10) NOT NULL DEFAULT '000',
   `gender` varchar(20) NOT NULL DEFAULT 'M',
@@ -93,10 +100,14 @@ CREATE TABLE `member` (
   `line_id` varchar(50) DEFAULT NULL,
   `prefix` varchar(100) DEFAULT NULL,
   `uuid_index` varchar(100) NOT NULL,
-  `member_role` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `member_role` varchar(10) DEFAULT NULL,
+  `data_sync` char(1) DEFAULT 'N' COMMENT 'ดึงข้อมูลไป local'
+);
+INSERT INTO `member`
+(code, company_code, gender, status, email, birthday, expired_date, total_purchase, mobile, point_expired_date, total_score, active, first_name, last_name, system_created, system_updated, line_id, prefix, uuid_index, member_role, data_sync)
+VALUES('MB00000', '000', 'M', 'S', 'softpos@gmail.com', '1980-09-08', '2030-09-23', 0.0, '0864108403', '2030-09-23', 100.0, 'Y', 'Admin', 'ผู้ดูแลระบบ', '2020-09-23 21:28:46.0', '2020-10-21 05:42:48.0', 'softpos', 'คุณ', 'ff3a447d-d10f-45a4-978b-bde5fa052123', 'super', 'N');
 
-CREATE TABLE `member_shipping` (
+CREATE TABLE IF NOT EXISTS `member_shipping` (
   `uuid_index` varchar(36) NOT NULL DEFAULT '',
   `map_latitude` varchar(50) DEFAULT NULL,
   `map_longitude` varchar(50) DEFAULT NULL,
@@ -111,9 +122,9 @@ CREATE TABLE `member_shipping` (
   `postcode` varchar(100) DEFAULT NULL,
   `member_lastname` varchar(100) DEFAULT NULL,
   `member_prefix` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `orders` (
+CREATE TABLE IF NOT EXISTS `orders` (
   `uuid_index` varchar(36) NOT NULL,
   `order_no` varchar(100) DEFAULT NULL,
   `cart_no` varchar(100) DEFAULT NULL,
@@ -140,10 +151,11 @@ CREATE TABLE `orders` (
   `order_update_date` datetime DEFAULT NULL,
   `order_create_date` datetime DEFAULT NULL,
   `order_status` varchar(50) DEFAULT NULL,
-  `signature` longtext
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `signature` longtext,
+  `member_mobile` varchar(10) DEFAULT NULL
+);
 
-CREATE TABLE `orders_detail` (
+CREATE TABLE IF NOT EXISTS `orders_detail` (
   `uuid_index` varchar(36) NOT NULL,
   `order_no` varchar(100) DEFAULT NULL,
   `product_code` varchar(20) DEFAULT NULL,
@@ -156,9 +168,9 @@ CREATE TABLE `orders_detail` (
   `special_text` varchar(100) DEFAULT NULL,
   `point` int(5) DEFAULT NULL,
   `cart_no` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `product` (
+CREATE TABLE IF NOT EXISTS `product` (
   `uuid_index` varchar(36) NOT NULL,
   `code` varchar(20) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
@@ -172,62 +184,85 @@ CREATE TABLE `product` (
   `price_d` float(10,2) DEFAULT NULL,
   `max_stock` int(5) DEFAULT NULL,
   `min_stock` int(5) DEFAULT NULL,
-  `unit_code_stock` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `unit_code_stock` varchar(10) DEFAULT NULL,
+  `qty_over_stock` varchar(1) DEFAULT 'N'
+);
 
-CREATE TABLE `promotion` (
+CREATE TABLE IF NOT EXISTS `promotion` (
   `uuid_index` varchar(36) NOT NULL,
   `product_code` varchar(100) DEFAULT NULL,
-  `product_name` varchar(100) DEFAULT NULL,
+  `redeem_name` varchar(100) DEFAULT NULL,
   `point_to_redeem` int(10) DEFAULT NULL,
   `start_time` datetime DEFAULT NULL,
   `finish_time` datetime DEFAULT NULL,
   `qty_in_stock` int(10) DEFAULT NULL,
-  `img_path` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `img_path` varchar(200) DEFAULT NULL,
+  `redeem_or_free` char(1) DEFAULT NULL,
+  `discount_amt` float(10,2) DEFAULT NULL,
+  `discount_percent` float(5,2) DEFAULT NULL
+);
 
-CREATE TABLE `redeem` (
+CREATE TABLE IF NOT EXISTS `redeem` (
   `uuid_index` varchar(36) NOT NULL,
-  `redeem_code` varchar(30) DEFAULT NULL,
-  `product_code` varchar(20) DEFAULT NULL,
-  `point_to_redeem` int(10) DEFAULT NULL,
-  `use_in_branch` varchar(30) DEFAULT NULL,
-  `emp_code_redeem` varchar(30) DEFAULT NULL,
-  `member_code_use` varchar(30) DEFAULT NULL,
-  `qty_in_use` int(3) DEFAULT NULL,
-  `stystem_create` datetime DEFAULT NULL,
-  `redeem_date` datetime DEFAULT NULL,
-  `in_time` datetime DEFAULT NULL,
-  `status_use` varchar(20) DEFAULT NULL,
-  `active` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `redeem_code` varchar(30) DEFAULT NULL COMMENT 'รหัส qr code',
+  `product_code` varchar(20) DEFAULT NULL COMMENT 'รหัสสินค้า',
+  `point_to_redeem` int(10) DEFAULT NULL COMMENT 'แต้มที่ลด',
+  `use_in_branch` varchar(30) DEFAULT NULL COMMENT 'สาขาที่ไปใช้',
+  `emp_code_redeem` varchar(30) DEFAULT NULL COMMENT 'รหัสพนักงาน',
+  `member_code_use` varchar(30) DEFAULT NULL COMMENT 'รหัสสมาชิก',
+  `qty_in_use` int(3) DEFAULT NULL COMMENT 'จำนวนที่ใช้',
+  `stystem_create` datetime DEFAULT NULL COMMENT 'วันที่สร้าง',
+  `redeem_date` datetime DEFAULT NULL COMMENT 'วันที่นำไปใช้',
+  `in_time` datetime DEFAULT NULL COMMENT 'ใช้ภายในระยะเวลา',
+  `status_use` varchar(20) DEFAULT NULL COMMENT 'คำอธิบาย สถานะ',
+  `active` varchar(1) DEFAULT NULL COMMENT 'flag  active',
+  `redeem_name` varchar(250) DEFAULT NULL COMMENT 'ชื่อการใช้งาน',
+  `bill_no` varchar(100) DEFAULT NULL COMMENT 'เลขที่เอกสาร',
+  `discount_amt` float(10,2) DEFAULT NULL COMMENT 'จำนวนเงินที่ลด',
+  `discount_percent` float(5,2) DEFAULT NULL COMMENT 'จำนวนเงินที่ลด เปอร์เซ็นต์',
+  `redeem_or_free` char(1) DEFAULT NULL COMMENT 'ประเภท ลด(R) หรือแถม(F)',
+  `data_sync` char(1) DEFAULT 'N' COMMENT 'ดึงข้อมูลไป local'
+);
 
-CREATE TABLE `roles` (
+CREATE TABLE IF NOT EXISTS `roles` (
   `uuid_index` varchar(36) NOT NULL,
   `code` varchar(20) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(100) DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL
+);
+INSERT INTO `roles`
+(uuid_index, code, name, `desc`)
+VALUES('37175ceb-220b-4dc1-9284-d220a792f395', 'admin', 'Admin', 'Administrator');
+INSERT INTO `roles`
+(uuid_index, code, name, `desc`)
+VALUES('d628fcbd-717a-4a0d-a842-537fe5b334a6', 'member', 'Member', 'Member register to branch');
+INSERT INTO `roles`
+(uuid_index, code, name, `desc`)
+VALUES('80bb217f-dd9e-4773-8023-eb13022507ae', 'employee', 'Employee', 'Employee to manage branch');
+INSERT INTO `roles`
+(uuid_index, code, name, `desc`)
+VALUES('c8b12dc4-28ad-4cfb-81eb-6317a140c949', 'super', 'Super Admin', 'Super Adminitrator');
 
-CREATE TABLE `stock` (
+CREATE TABLE IF NOT EXISTS `stock` (
   `uuid_index` varchar(36) NOT NULL,
   `code` varchar(5) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `stock_product` (
+CREATE TABLE IF NOT EXISTS `stock_product` (
   `uuid_index` varchar(36) NOT NULL,
   `stock_code` varchar(5) DEFAULT NULL,
   `product_code` varchar(20) DEFAULT NULL,
   `in_stock` int(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `sytem_config` (
+CREATE TABLE IF NOT EXISTS `sytem_config` (
   `index_uuid` varchar(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE `table_crud` (
+CREATE TABLE IF NOT EXISTS `table_crud` (
   `uuid_index` varchar(36) NOT NULL,
   `col1` varchar(100) DEFAULT NULL,
   `col2` varchar(100) DEFAULT NULL,
   `col3` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
