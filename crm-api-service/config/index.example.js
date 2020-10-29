db5fd6f1734fea748e3935aedd1615fde9c6a004
นที สังข์ทองงาม <natee.live@gmail.com>
2020-10-30 00:46:34 +0700

    config crm-api-service improvement

---
+++ crm-api-service/config/index.example.js
@@ -0,0 +1,13 @@
+const config = {
+  host: "localhost",
+  user: "root",
+  password: "myPassword",
+  port: "3306",
+  connectionLimit: 5,
+  connectTimeout: 10000,
+  acquireTimeout: 10000,
+  waitForConnections: true,
+  queueLimit: 0,
+}
+
+module.exports = config
