f67fb08615cb5bd6971e307c2223b8a687b434b6
นที สังข์ทองงาม <natee.live@gmail.com>
2020-11-07 22:09:55 +0700

    add auth router

--- webdialy-online/app/containers/App/selectors.js
+++ webdialy-online/app/containers/App/selectors.js
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect';
+import { initialState } from './reducer';
 
 const selectRouter = state => state.router;
+const selectAppDomain = state => state.app || initialState;
 
 const makeSelectLocation = () =>
   createSelector(
@@ -8,4 +10,16 @@ const makeSelectLocation = () =>
     routerState => routerState.location,
   );
 
-export { makeSelectLocation };
+const makeSelectApp = () =>
+  createSelector(
+    selectAppDomain,
+    substate => substate,
+  );
+
+const makeSelectLeftMenu = () =>
+  createSelector(
+    selectAppDomain,
+    substate => substate.leftMenu,
+  );
+
+export { makeSelectLocation, makeSelectApp, makeSelectLeftMenu };
