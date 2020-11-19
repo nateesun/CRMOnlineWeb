8de903c570e7925b5a3f48e031eab8b88e90b56b
Nathee Sungthong-ngam <natee.live@gmail.com>
2020-11-19 15:56:32 +0700

    remove sourcecode

--- member-monitor/monitor-server/utils/testUtil.js
+++
@@ -1,4 +0,0 @@
-const TextUtil = require('./TextUtil')
-
-console.log('result:', TextUtil.convUnicode2Ascii('Nathee Sungthongngam'))
-// console.log('result:', TextUtil.convAscii2Unicode('¹·Õ ÊÑ§¢ì·Í§§ÒÁ'))
