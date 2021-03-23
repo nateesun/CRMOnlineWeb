7dbb7be208ace2ff95df8ef5386f92cc77dcd035
Nathee Sungthong-ngam <natee.live@gmail.com>
2021-03-23 21:01:36 +0700

    remove unused file

--- webdialy-online/app/containers/AddressShipping/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectAddressShippingDomain } from '../selectors';
-
-describe('selectAddressShippingDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
