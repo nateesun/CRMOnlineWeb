2c33218cb331cf927787e43502f028008e50c54f
Nathee Sungthong-ngam <natee.live@gmail.com>
2021-04-01 11:03:00 +0700

    remove socket io client

---
+++ crm-api-service/utils/socketApi.js
@@ -0,0 +1,14 @@
+const io = require( "socket.io" )({
+    cors: '*',
+});
+const socketapi = {
+    io: io
+};
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+    console.log( "A user connected" );
+});
+// end of socket.io logic
+
+module.exports = socketapi;
