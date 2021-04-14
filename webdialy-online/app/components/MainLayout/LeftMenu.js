47c1749311084f5393355e2b9736ad91c896961d
Nathee Sungthong-ngam <natee.live@gmail.com>
2021-04-14 11:08:43 +0700

    collapse config menu

--- webdialy-online/app/components/MainLayout/LeftMenu.js
+++ webdialy-online/app/components/MainLayout/LeftMenu.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FormattedMessage } from 'react-intl';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -11,8 +14,9 @@ import PeopleIcon from '@material-ui/icons/People';
 import LockIcon from '@material-ui/icons/Lock';
 import RecentActorsIcon from '@material-ui/icons/RecentActors';
 import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
-import List from '@material-ui/core/List';
-import { FormattedMessage } from 'react-intl';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import ButtonLink from 'components/ButtonLink';
 
 const useStyles = makeStyles(() => ({
@@ -24,37 +28,88 @@ const useStyles = makeStyles(() => ({
 export default function LeftMenu(props) {
   const classes = useStyles();
   const { leftMenu = [], appConstants, scope, title } = props;
+  const [open, setOpen] = useState(false);
+
+  const leftMenuMaster = leftMenu.filter(
+    item => item.menu_id === 'Master' || item.menu_id === 'Settings',
+  );
+  const leftMenuOther = leftMenu.filter(
+    item => item.menu_id !== 'Master' && item.menu_id !== 'Settings',
+  );
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    if (
+      [
+        'Database',
+        'Promotion',
+        'Roles',
+        'Branch',
+        'Company',
+        'Product',
+        'Stock',
+      ].includes(title)
+    ) {
+      setOpen(true);
+    }
+  }, []);
 
   return (
     <List style={{ background: '#fbf9f8' }}>
-      <React.Fragment>
-        {leftMenu.map(({ id, icon, to_path: to }) => (
-          <ButtonLink to={appConstants.publicPath + to} key={`menu${id}`}>
-            <ListItem
-              key={id}
-              button
-              className={clsx(id === title && classes.itemActiveItem,
+      {leftMenuOther.map(({ id, icon, to_path: to }) => (
+        <ButtonLink to={appConstants.publicPath + to} key={`menu${id}`}>
+          <ListItem
+            key={id}
+            button
+            className={clsx(id === title && classes.itemActiveItem)}
+          >
+            <FormattedMessage id={`${scope}.menu${id}`}>
+              {title => (
+                <ListItemIcon title={title}>
+                  {icon === 'DnsRoundedIcon' && <DnsRoundedIcon />}
+                  {icon === 'CardGiftcardIcon' && <CardGiftcardIcon />}
+                  {icon === 'LocalMallIcon' && <LocalMallIcon />}
+                  {icon === 'PeopleIcon' && <PeopleIcon />}
+                  {icon === 'LockIcon' && <LockIcon />}
+                  {icon === 'RecentActorsIcon' && <RecentActorsIcon />}
+                </ListItemIcon>
               )}
-            >
-              <FormattedMessage id={`${scope}.menu${id}`}>
-                {title => (
-                  <ListItemIcon title={title}>
-                    {icon === 'DnsRoundedIcon' && <DnsRoundedIcon />}
-                    {icon === 'CardGiftcardIcon' && <CardGiftcardIcon />}
-                    {icon === 'LocalMallIcon' && <LocalMallIcon />}
-                    {icon === 'PeopleIcon' && <PeopleIcon />}
-                    {icon === 'LockIcon' && <LockIcon />}
-                    {icon === 'RecentActorsIcon' && <RecentActorsIcon />}
-                  </ListItemIcon>
-                )}
-              </FormattedMessage>
-              <ListItemText>
-                <FormattedMessage id={`${scope}.menu${id}`} />
-              </ListItemText>
-            </ListItem>
-          </ButtonLink>
-        ))}
-      </React.Fragment>
+            </FormattedMessage>
+            <ListItemText>
+              <FormattedMessage id={`${scope}.menu${id}`} />
+            </ListItemText>
+          </ListItem>
+        </ButtonLink>
+      ))}
+      <Divider />
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <DnsRoundedIcon />
+        </ListItemIcon>
+        <ListItemText primary="ข้อมูลระบบ" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" style={{ marginLeft: '55px' }}>
+          {leftMenuMaster.map(({ id, icon, to_path: to }) => (
+            <ButtonLink to={appConstants.publicPath + to} key={`menu${id}`}>
+              <ListItem
+                key={id}
+                button
+                className={clsx(id === title && classes.itemActiveItem)}
+              >
+                <ListItemText>
+                  <FormattedMessage id={`${scope}.menu${id}`} />
+                </ListItemText>
+              </ListItem>
+            </ButtonLink>
+          ))}
+        </List>
+      </Collapse>
+      <Divider />
     </List>
   );
 }
