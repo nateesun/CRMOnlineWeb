f87d3d6e92514d84ae247f13daf6f7280f139d8d
Nathee Sungthong-ngam <natee.live@gmail.com>
2021-04-24 07:37:57 +0700

    remove index.test.js in containers folder

--- webdialy-online/app/components/ToggleOption/tests/index.test.js
+++ webdialy-online/app/components/ToggleOption/tests/index.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+
+import history from 'utils/history';
+import LanguageProvider from 'containers/LanguageProvider';
+import configureStore from 'configureStore';
+import { translationMessages } from 'i18n';
 
 import ToggleOption from '../index';
 
+const initialState = {};
+const store = configureStore(initialState, history);
+
 describe('ToggleOption components', () => {
   it('test snapshot', () => {
-    const component = renderer.create(<ToggleOption />);
+    const component = renderer.create(
+      <Provider store={store}>
+        <LanguageProvider messages={translationMessages}>
+          <ToggleOption value="en" />
+        </LanguageProvider>
+      </Provider>,
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
